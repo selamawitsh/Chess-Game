8f95e35f436b154331fb45853edecc79b98d1485
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-06-18 21:20:30 +0300

    selecting a piece by clicking

--- chess-game/src/components/ChessBoard.jsx
+++ chess-game/src/components/ChessBoard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Square from './Square'
 import '../styles/ChessBoard.css' 
+import { useState } from 'react';
+
 function ChessBoard() {
    
-const initialBoard = [
+    const initialBoard = [
     ['fas fa-chess-rook', 'fas fa-chess-knight', 'fas fa-chess-bishop', 'fas fa-chess-queen', 'fas fa-chess-king', 'fas fa-chess-bishop', 'fas fa-chess-knight', 'fas fa-chess-rook'],
     Array(8).fill('fas fa-chess-pawn'),
     Array(8).fill(null),
@@ -12,7 +14,7 @@ const initialBoard = [
     Array(8).fill(null),
     Array(8).fill('fas fa-chess-pawn white'),
     ['fas fa-chess-rook white', 'fas fa-chess-knight white', 'fas fa-chess-bishop white', 'fas fa-chess-queen white', 'fas fa-chess-king white', 'fas fa-chess-bishop white', 'fas fa-chess-knight white', 'fas fa-chess-rook white']
-  ];
+    ];
 
     //   for (let row=0; row<8 ; row++) {
     //     for (let col=0; col<8; col++){
@@ -22,18 +24,37 @@ const initialBoard = [
     //         );
     //     }
     //   }
+
+    const [board, setBoard] = useState(initialBoard);
+    const [selectedSquare, setSelectedSquare] = useState(null); 
+
+    function handleSquareClick(row, col) {
+        const piece = board[row][col];
+
+        if (piece){
+            setSelectedSquare({row,col});
+        }
+        else{
+            setSelectedSquare(null);
+        }
+    }
+
            
   return (
     <div className="board">
         {
-            initialBoard.map((row, i)=>
+            board.map((row, i)=>
             row.map((piece,j)=>{
                 const isDark= (i+j) %2 ===1;
+                const isSelected = selectedSquare?.row === i && selectedSquare?.col === j;
+
                 return(
                     <Square 
                         isDark={isDark}
                         iconClass={piece}
                         key={`${i}-${j}`}
+                        onClick={() => handleSquareClick(i, j)}
+                        isSelected={isSelected}
                     />
                 )
             })
--- chess-game/src/components/Square.jsx
+++ chess-game/src/components/Square.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function Square({ isDark, iconClass }) {
-  return (
-    <div className ={`square ${isDark ? 'dark': 'light'}`}>
-    {iconClass && <i className={`${iconClass} piece`} > </i>}
-    </div>
-  )
+function Square({ isDark, iconClass, onClick, isSelected }) {
+    return (
+        <div
+        className={`square ${isDark ? 'dark' : 'light'} ${isSelected ? 'selected' : ''}`}
+        onClick={onClick}
+        >
+        {iconClass && <i className={`${iconClass} piece`}></i>}
+        </div>
+    );
 }
 
 export default Square
